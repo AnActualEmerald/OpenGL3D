4a0c481b15c0e0919103e401ee9c1c9ce8ab7c6d
Burrito119 <kevinliger13@gmail.com>
2014-02-03 13:29:35 -0500

    Fixed typo

--- OpenGLFun/screen.h
+++ OpenGLFun/screen.h
@@ -8,7 +8,7 @@ public:
 	static void drawRect(float x, float y, float width, float height);
 	static void setColor(float r, float g, float b);
 	static void drawCube(float x, float y, float z);
-	static void drawCube(float x, flaot y, float z, float w, float h);
+	static void drawCube(float x, float y, float z, float w, float h);
 
 private:
 
